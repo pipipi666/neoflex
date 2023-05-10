8b99e79041984258169959b8860b38f47291a636
pipipi666 <nnm107072@gmail.com>
2023-05-10 19:09:41 +0300

    feat: add 404 page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,3 +1,10 @@
+import styles from "./not-found.module.scss";
+
 export const NotFoundPage = () => {
-  return <div>404</div>;
+  return (
+    <div className={styles.container}>
+      <p className={styles.code}>404</p>
+      <p className={styles.message}>Страница не найдена</p>
+    </div>
+  );
 };
