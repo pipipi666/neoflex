340b0f57da5562ad0c1258da38004e5faf8b5d43
pipipi666 <nnm107072@gmail.com>
2023-05-10 12:26:24 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ROUTES } from "consts/routes";
+import { Header } from "components/header/header";
+import { Footer } from "components/footer/footer";
+import { MainPage } from "pages/main/main";
+import { CartPage } from "pages/cart/cart";
+import { NotFoundPage } from "pages/not-found/not-found";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path={ROUTES.HOME} element={<MainPage />} />
+        <Route path={ROUTES.CART} element={<CartPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
